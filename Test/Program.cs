68093b879e7600dfac01e063e60ac7a56339ae56
MrGodOfNight <DenRussianBoy@mail.ru>
2025-04-06 01:13:26 +0500

    first quad

---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using LycorisEngine;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1();
+            a.Main();
+        }
+    }
+}
